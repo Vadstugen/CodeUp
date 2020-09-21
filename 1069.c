324a171198687460f4eae97d78a2f308dbcbcd6f
VadStugen <53822162+Vadstugen@users.noreply.github.com>
2020-09-21 13:21:49 +0900

    Create 1069.c

---
+++ 1069.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    char num;
+    scanf("%c", &num);
+    switch(num){
+    	case 'A':
+    		printf("best!!!");
+    		break;
+    	case 'B':
+    		printf("good!!");
+    		break;
+    	case 'C':
+    		printf("run!");
+    		break;
+    	case 'D':
+    		printf("slowly~");
+    		break;
+    	default:
+    		printf("what?");
+    		
+	}
+}
