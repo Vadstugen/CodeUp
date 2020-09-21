a95d82b409aa3b0064a871055b19133baa3dd3f1
VadStugen <53822162+Vadstugen@users.noreply.github.com>
2020-09-21 10:54:47 +0900

    Create 1025.c

---
+++ 1025.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+	int n1, n2, n3, n4, n5;
+	scanf("%1d%1d%1d%1d%1d",&n1, &n2, &n3, &n4, &n5);
+	printf("[%d]\n[%d]\n[%d]\n[%d]\n[%d]",n1*10000,n2*1000,n3*100,n4*10,n5);
+}
