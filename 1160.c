f08bddb624e188cda1eebba477d8343eb251b06f
VadStugen <53822162+Vadstugen@users.noreply.github.com>
2020-09-23 10:19:29 +0900

    Create 1160.c

---
+++ 1160.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    switch(n){
+        case 1://월
+        printf("oh my god");
+        break;
+        
+        case 2://화
+        printf("enjoy");
+        break;
+        
+        case 3://수
+        printf("oh my god");
+        break;
+        
+        case 4://목
+        printf("enjoy");
+        break;
+        
+        case 5://금
+        printf("oh my god");
+        break;
+        
+        case 6:
+        printf("enjoy");
+        break;
+        
+        case 7://일
+        printf("oh my god");
+        break;
+        
+        default:
+        printf("enjoy");
+    }
+    return 0;
+}
