f55b8073ee6d2a1bb5ab10ff61fae995ba66aa7f
VadStugen <53822162+Vadstugen@users.noreply.github.com>
2020-09-23 10:17:19 +0900

    Create 1150.c

---
+++ 1150.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    int n1, n2, n3;
+    scanf("%d %d %d", &n1, &n2, &n3);
+    if(n1 <= n2){
+        if(n1 <= n3){
+            printf("%d", n1);
+            return 0;
+        }
+    }
+    if(n2 <= n1){
+        if(n2 <= n3){
+            printf("%d", n2);
+            return 0;
+        }
+    }
+    if(n3 <= n1){
+        if(n3 <= n2){
+            printf("%d", n3);
+            return 0;
+        }
+    }
+}
