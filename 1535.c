c57ff030aba27af7bb4d6408c2f4d5f5deb91c43
VadStugen <53822162+Vadstugen@users.noreply.github.com>
2020-10-04 22:57:56 +0900

    Create 1535.c

---
+++ 1535.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int n, d[110];
+
+
+int f(){
+    int max = 0;
+    int idx = 0;
+    for(int i=0; i<n; i++){
+        if(d[i] > max){
+            max = d[i];
+            idx = i;
+        }
+    }
+    return idx+1;
+}
+int main()
+{
+  scanf("%d", &n);
+
+  for(int i=0; i<n; i++)
+    scanf("%d", &d[i]);
+
+  printf("%d", f());
+  return 0;
+}
