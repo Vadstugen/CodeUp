edfb284ab3da0e0c41544ac665ce7f2dbad41681
VadStugen <53822162+Vadstugen@users.noreply.github.com>
2020-10-05 10:58:22 +0900

    Create 1546.c

---
+++ 1546.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int n;
+
+
+
+int zero(int n){
+    if(n == 0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int plus(int n){
+    if(n > 0){
+        return 1;
+    }
+    else if(n < 0){
+        return 0;
+    }
+}
+int main()
+{
+  scanf("%d", &n);
+  if(zero(n)) printf("zero");
+  else printf("%s", plus(n)?"plus":"minus");
+  return 0;
+}
