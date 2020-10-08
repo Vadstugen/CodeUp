103d5bf17a2df9fa023aa6b90a25b07d34a7fa23
VadStugen <53822162+Vadstugen@users.noreply.github.com>
2020-10-08 23:33:00 +0900

    Create 1551.c

---
+++ 1551.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int n, d[100010], k;
+
+int f(){
+    for(int i=1; i<=n; i++){
+        if(d[i] == k){
+            return i;
+        }
+        if(i == n){
+            return -1;
+        }
+    }
+}
+
+int main()
+{
+  scanf("%d", &n);
+
+  for(int i=1; i<=n; i++)
+    scanf("%d", &d[i]);
+
+  scanf("%d", &k);
+  printf("%d\n", f(k));
+}
