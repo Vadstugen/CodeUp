edda43338e6e630349e3756adc6b55f924a260e5
VadStugen <53822162+Vadstugen@users.noreply.github.com>
2020-10-23 23:49:26 +0900

    Create 1463.c

---
+++ 1463.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    int arr[n][n];
+    int num = 1;
+    for(int i=0; i<n; i++){
+        for(int j=n-1; j>=0; j--){
+            arr[j][i] = num;
+            num++;
+        }
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
