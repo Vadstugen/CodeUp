0e816a3706960000b8ee4274b09f10f97c2b7c59
VadStugen <53822162+Vadstugen@users.noreply.github.com>
2020-11-18 20:44:36 +0900

    Create 1406.java

---
+++ 1406.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        if(str.equals("love")){
+            System.out.println("I love you.");
+        }
+    }
+}
