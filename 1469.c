f4ec9964f2af2d399ecdc3dfb927dea9acde260e
Soy chicken <53822162+Vadstugen@users.noreply.github.com>
2020-11-28 21:50:51 +0900

    Create 1469.c

---
+++ 1469.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    int n;
+    int num = 1;
+    scanf("%d", &n);
+    int arr[n][n];
+    for(int i=0; i<n; i++){
+        if(i % 2 == 0){
+            for(int j=n-1; j>=0; j--){
+                arr[i][j] = num++;
+            }
+        }
+        if(i % 2 == 1){
+            for(int j=0; j<n; j++){
+                arr[i][j] = num++;
+            }
+        }
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+}
